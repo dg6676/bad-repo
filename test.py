b98347b2da91bfdc46053c8242d56f7d4466c1c1
Hoyoung Jung <honux77@gmail.com>
2017-01-31 12:01:56 +0900

    Very Hard...
    I am very happy becuase my code works!
    I am very happy becuase my code works!
    I am very happy becuase my code works!
    I am very happy becuase my code works!
    I am very happy becuase my code works!

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
 print ("Yet another hello")
+
+map = '''\
++--------+
+|        |
+|        |
+|     P  |
++--------+'''
+
+print(map)
+
+arr = []
+for i in range(5):
+    arr.append([])
+    for j in range(10):
+        arr[i].append(map[i * 11 + j])
+
+for arr1 in (arr):
+    print(arr1)
